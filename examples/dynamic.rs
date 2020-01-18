0ea7fa540f3b8aeb6de3c9b7f0f7f6f1bb5d8bab
Alexandre Bury <alexandre.bury@gmail.com>
2020-01-17 17:04:31 -0800

    Update cursive to 0.14.0

--- examples/dynamic.rs
+++ examples/dynamic.rs
@@ -1,11 +1,11 @@
+use cursive::view::{Nameable, Resizable};
+use cursive::views::{DummyView, Panel, ResizedView};
 use cursive::Cursive;
-use cursive::view::{Boxable, Identifiable};
-use cursive::views::{Panel, DummyView, BoxView};
 use cursive_aligned_view::{Alignable, AlignedView};
 
 use std::time::Duration;
 
-type ChildView = BoxView<Panel<DummyView>>;
+type ChildView = ResizedView<Panel<DummyView>>;
 
 fn main() {
     let mut siv = Cursive::default();
@@ -14,78 +14,79 @@ fn main() {
         .title("Hello, world!")
         .fixed_width(20)
         .align_top_left()
-        .with_id("panel");
+        .with_name("panel");
 
     let sink = siv.cb_sink().clone();
     std::thread::spawn(move || {
         std::thread::sleep(Duration::from_secs(1));
         sink.send(Box::new(|siv| {
-            siv.call_on_id(
-                "panel",
-                |view: &mut AlignedView<ChildView>| view.set_top_center(),
-            );
-        })).expect("alignment change failed");
+            siv.call_on_name("panel", |view: &mut AlignedView<ChildView>| {
+                view.set_top_center()
+            });
+        }))
+        .expect("alignment change failed");
 
         std::thread::sleep(Duration::from_secs(1));
         sink.send(Box::new(|siv| {
-            siv.call_on_id(
-                "panel",
-                |view: &mut AlignedView<ChildView>| view.set_top_right(),
-            );
-        })).expect("alignment change failed");
+            siv.call_on_name("panel", |view: &mut AlignedView<ChildView>| {
+                view.set_top_right()
+            });
+        }))
+        .expect("alignment change failed");
 
         std::thread::sleep(Duration::from_secs(1));
         sink.send(Box::new(|siv| {
-            siv.call_on_id(
-                "panel",
-                |view: &mut AlignedView<ChildView>| view.set_center_left(),
-            );
-        })).expect("alignment change failed");
+            siv.call_on_name("panel", |view: &mut AlignedView<ChildView>| {
+                view.set_center_left()
+            });
+        }))
+        .expect("alignment change failed");
 
         std::thread::sleep(Duration::from_secs(1));
         sink.send(Box::new(|siv| {
-            siv.call_on_id(
-                "panel",
-                |view: &mut AlignedView<ChildView>| view.set_center(),
-            );
-        })).expect("alignment change failed");
+            siv.call_on_name("panel", |view: &mut AlignedView<ChildView>| {
+                view.set_center()
+            });
+        }))
+        .expect("alignment change failed");
 
         std::thread::sleep(Duration::from_secs(1));
         sink.send(Box::new(|siv| {
-            siv.call_on_id(
-                "panel",
-                |view: &mut AlignedView<ChildView>| view.set_center_right(),
-            );
-        })).expect("alignment change failed");
+            siv.call_on_name("panel", |view: &mut AlignedView<ChildView>| {
+                view.set_center_right()
+            });
+        }))
+        .expect("alignment change failed");
 
         std::thread::sleep(Duration::from_secs(1));
         sink.send(Box::new(|siv| {
-            siv.call_on_id(
-                "panel",
-                |view: &mut AlignedView<ChildView>| view.set_bottom_left(),
-            );
-        })).expect("alignment change failed");
+            siv.call_on_name("panel", |view: &mut AlignedView<ChildView>| {
+                view.set_bottom_left()
+            });
+        }))
+        .expect("alignment change failed");
 
         std::thread::sleep(Duration::from_secs(1));
         sink.send(Box::new(|siv| {
-            siv.call_on_id(
-                "panel",
-                |view: &mut AlignedView<ChildView>| view.set_bottom_center(),
-            );
-        })).expect("alignment change failed");
+            siv.call_on_name("panel", |view: &mut AlignedView<ChildView>| {
+                view.set_bottom_center()
+            });
+        }))
+        .expect("alignment change failed");
 
         std::thread::sleep(Duration::from_secs(1));
         sink.send(Box::new(|siv| {
-            siv.call_on_id(
-                "panel",
-                |view: &mut AlignedView<ChildView>| view.set_bottom_right(),
-            );
-        })).expect("alignment change failed");
+            siv.call_on_name("panel", |view: &mut AlignedView<ChildView>| {
+                view.set_bottom_right()
+            });
+        }))
+        .expect("alignment change failed");
 
         std::thread::sleep(Duration::from_secs(1));
         sink.send(Box::new(|siv| {
             siv.quit();
-        })).expect("alignment change failed");
+        }))
+        .expect("alignment change failed");
     });
 
     siv.add_fullscreen_layer(panel);
